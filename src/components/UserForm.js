0bdf6ecb96828dc59b5895f0fafd3d3637cf2fa3
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-04-09 18:31:39 +0100

    new

---
+++ src/components/UserForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails';
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    //Proceed to the nest step 
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+    //Go Back to the prev step 
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    // Handle field Change
+      handleChange = input => e => {
+        this.setState({[input]: e.target.value})
+      }
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = {firstName, lastName, email, occupation, city, bio}
+    // eslint-disable-next-line default-case
+    switch(step) {
+        case 1: 
+        return (
+            <FormUserDetails 
+            nextStep = {this.nextStep}
+            handleChange = {this.handleChange}
+            values = {values}
+            />
+        )
+        case 2:
+            return <h1>FormPersonalDetails</h1>
+            case 3: 
+            return <h1>Confirm</h1>
+            case 4: 
+            return <h1>Success</h1>
+    }
+  }
+}
+
+export default UserForm
